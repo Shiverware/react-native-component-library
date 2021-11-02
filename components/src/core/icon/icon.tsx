bac2d4828d2b9d2b812d2923b5ca02b8ba638874
emclaug2 <evan.w.mclaughlin@gmail.com>
2021-11-02 13:02:17 -0400

    Start BLUI migration

--- components/src/core/icon/icon.tsx
+++ components/src/core/icon/icon.tsx
@@ -3,7 +3,7 @@ import { Image, I18nManager, Platform, ImageSourcePropType } from 'react-native'
 import { IconComponentProps, IconFamily, IconSource, IconSourceBase } from '../__types__';
 import MatIcon from 'react-native-vector-icons/MaterialIcons';
 import MatCommunity from 'react-native-vector-icons/MaterialCommunityIcons';
-import PXBIcon from '@pxblue/react-native-vector-icons';
+import BLUIIcon from '@brightlayer-ui/react-native-vector-icons';
 import { Body1 } from '../typography';
 import { useTheme } from 'react-native-paper';
 
@@ -73,7 +73,7 @@ export const Icon: React.FC<IconProps> = (props) => {
                 );
             case 'pxblue':
                 return (
-                    <PXBIcon
+                    <BLUIIcon
                         name={source.name}
                         size={size}
                         allowFontScaling={scale}
@@ -81,7 +81,6 @@ export const Icon: React.FC<IconProps> = (props) => {
                         style={flip ? flipIconStyle : {}}
                     />
                 );
-            case 'material':
             default:
                 return (
                     <MatIcon
